051fc4b3f95bf3fe78cd55b5f5907d582a8120b8
Dyuthi <135154834+dyuhti@users.noreply.github.com>
2025-04-27 12:45:58 +0530

    main.py

--- Renting Capstone project/main.py
+++ Renting Capstone project/main.py
@@ -7,8 +7,8 @@ from selenium.webdriver.chrome.service import Service
 from selenium import webdriver
 
 CHROME_DRIVER_PATH = "C:\Development\chromedriver.exe"
-GOOGLE_FORM = "https://docs.google.com/forms/d/e/1FAIpQLScmna0s6e-Ft_tU1YefxEqSdyGB_AGvDTrrz3nzREj0xF7ZHA/viewform?usp=sf_link"
-ZILLOW = "https://www.zillow.com/san-francisco-ca/rentals/?searchQueryState=%7B%22pagination%22%3A%7B%7D%2C%22mapBounds%22%3A%7B%22north%22%3A37.842914%2C%22east%22%3A-122.32992%2C%22south%22%3A37.707608%2C%22west%22%3A-122.536739%7D%2C%22mapZoom%22%3A11%2C%22isMapVisible%22%3Afalse%2C%22filterState%22%3A%7B%22price%22%3A%7B%22max%22%3A872627%7D%2C%22beds%22%3A%7B%22min%22%3A1%7D%2C%22fore%22%3A%7B%22value%22%3Afalse%7D%2C%22mp%22%3A%7B%22max%22%3A3000%7D%2C%22auc%22%3A%7B%22value%22%3Afalse%7D%2C%22nc%22%3A%7B%22value%22%3Afalse%7D%2C%22fr%22%3A%7B%22value%22%3Atrue%7D%2C%22fsbo%22%3A%7B%22value%22%3Afalse%7D%2C%22cmsn%22%3A%7B%22value%22%3Afalse%7D%2C%22fsba%22%3A%7B%22value%22%3Afalse%7D%7D%2C%22isListVisible%22%3Atrue%2C%22regionSelection%22%3A%5B%7B%22regionId%22%3A20330%2C%22regionType%22%3A6%7D%5D%7D"
+GOOGLE_FORM = "https://docs.google.com/forms/****"
+ZILLOW = "https://www.zillow.com/san-francisco-ca/rentals/****"
 headers = {
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_1) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.2 Safari/605.1.15",
     "Accept-Language": "en-US"
